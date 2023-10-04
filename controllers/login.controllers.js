91b7852ef8427350038c285c921b3e836c47859d
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-10-04 16:46:36 +0200

    test change login controlers

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -14,3 +14,25 @@ module.exports.getPasswordValid = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+module.exports.getUser = async (req, res) => {
+    const { login, pass} = req.query;
+
+    try {
+        const user= await loginModels.find({
+            login: login,
+            pass: pass
+        });
+
+        if (user.length > 0) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "Aucun user trouvé avec ce login et pass." });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Erreur interne du serveur." });
+    }
+}
+
+
+router.get("getUser", getUser )
